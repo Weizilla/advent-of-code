d7bd2a7f888f2e1a17ad322ca8ec983c09d08a6d
Wei Yang <wei@weizilla.com>
2019-02-15 21:24:10 -0600

    More Day 6

--- haskell/src/Day06.hs
+++ haskell/src/Day06.hs
@@ -11,7 +11,8 @@ e = ["1, 1",
     "5, 5",
     "8, 9"]
 
-data Point = Point Integer Integer deriving (Show)
+data Point = Point {pX :: Integer, pY :: Integer}
+data Bounds = Bounds {minX :: Integer, minY :: Integer, maxX :: Integer, maxY :: Integer}
 
 parsePoint :: Parser Point
 parsePoint = (\a b -> Point (read a) (read b)) <$> many digit <* string ", " <*> many digit
@@ -21,3 +22,19 @@ parseInput input = case mapM (parse parsePoint "") input of
     Right xs -> xs
     Left e -> error $ "Error parsing" ++ show e
 
+bounds :: [Point] -> Bounds
+bounds ps =
+    let
+        maxX = maximum . map pX $ ps
+        maxY = maximum . map pY $ ps
+        minX = minimum . map pX $ ps
+        minY = minimum . map pY $ ps
+    in
+        Bounds minX minY maxX maxY
+
+inBounds :: Point -> Bounds -> Bool
+inBounds p b = or [
+    pX p == minX b,
+    pY p == minY b,
+    pX p == maxX b,
+    pY p == maxY b]
