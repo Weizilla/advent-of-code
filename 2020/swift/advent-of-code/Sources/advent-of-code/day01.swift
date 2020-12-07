0ab8692b3bc036c19075b84daeb6cf06b6fee0d1
Wei Yang <wei@weizilla.com>
2020-12-07 07:38:37 -0600

    More day 1

--- 2020/swift/advent-of-code/Sources/advent-of-code/day01.swift
+++ 2020/swift/advent-of-code/Sources/advent-of-code/day01.swift
@@ -1,7 +1,40 @@
 import Foundation
 
+let test1Input = [
+    1721,
+    979,
+    366,
+    299,
+    675,
+    1456,
+]
+
+let part1Expected = 514579
+let part2Expected = 241861950
+
 func day01() -> Int {
-    print(readIntInput(1))
-    
-    return 0
+//    part1()
+    return part2()
+}
+
+func part1() -> Int {
+    let inputs: Set = Set(readIntInput(1))
+    for input in inputs {
+        let other = 2020 - input
+        if inputs.contains(other) {
+            return input * other
+        }
+    }
+    fatalError("Not found")
+}
+
+func part2() -> Int {
+    let inputs: Set = Set(readIntInput(1))
+    for input in inputs {
+        let other = 2020 - input
+        if inputs.contains(other) {
+            return input * other
+        }
+    }
+    fatalError("Not found")
 }
