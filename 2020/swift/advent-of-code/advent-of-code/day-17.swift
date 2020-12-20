8edd31297ccd40414c80dcfc4f9ef597b4b7b3be
Wei Yang <wei@weizilla.com>
2020-12-20 00:08:41 -0600

    More day 17

--- 2020/swift/advent-of-code/advent-of-code/day-17.swift
+++ 2020/swift/advent-of-code/advent-of-code/day-17.swift
@@ -1,35 +1,73 @@
 import Foundation
 
 func day17Part1() -> Int {
-    let input = readInput(17, example: 1)
+    let input = readInput(17)
     // z, y, x
-    var grid = Table()
+    var grid = Grid()
     for y in 0..<input.count {
         for x in 0..<input[0].count {
             grid.set(x, y, 0, Array(input[y])[x])
         }
     }
     print(grid)
-    return 0
+
+    for i in 0..<6 {
+        var newGrid = Grid()
+        print(" \(i) ----------")
+        for z in grid.zBounds[0]-1...grid.zBounds[1]+1 {
+            for y in grid.yBounds[0]-1...grid.yBounds[1]+1 {
+                for x in grid.xBounds[0]-1...grid.xBounds[1]+1 {
+                    let numA = numActiveNeighbors(grid: &grid, point: [x, y, z])
+//                    print("\([x, y, z]) \(numA)")
+                    if (grid.get(x, y, z) == "#") {
+                        if (numA == 2 || numA == 3) {
+                            newGrid.set(x, y, z, "#")
+                        } else {
+                            newGrid.set(x, y, z, ".")
+                        }
+                    } else if (grid.get(x, y, z) == ".") {
+                        if (numA == 3) {
+                            newGrid.set(x, y, z, "#")
+                        } else {
+                            newGrid.set(x, y, z, ".")
+                        }
+                    }
+                }
+            }
+        }
+//        print(newGrid)
+        grid = newGrid
+    }
+
+    return grid.numActive
 }
 
-class Table: CustomStringConvertible {
-    var table: [Int: [Int: [Int: Character]]] = [:]
+func numActiveNeighbors(grid: inout Grid, point: [Int]) -> Int {
+    var num = 0
+    for x in -1...1 {
+        for y in -1...1 {
+            for z in -1...1 {
+                if (!(x == 0 && y == 0 && z == 0) && grid.get(x + point[0], y + point[1], z + point[2]) == "#") {
+                    num += 1
+                }
+            }
+        }
+    }
+    return num
+}
+
+class Grid: CustomStringConvertible {
+    var grid: [[Int]: Character] = [:]
     var xBounds = [0, 0]
     var yBounds = [0, 0]
     var zBounds = [0, 0]
+    private var _numActive = 0
 
     func set(_ x: Int, _ y: Int, _ z: Int, _ value: Character) {
-        if var xt = table[x] {
-            if var yt = xt[y] {
-                yt[z] = value
-            } else {
-                xt[y] = [z: value]
-                table[x] = xt
-            }
-        } else {
-            table[x] = [y:[z: value]]
-        }
+        let key = [x, y, z]
+        grid[key] = value
+
+        _numActive += value == "#" ? 1 : 0
 
         xBounds = [min(x, xBounds[0]), max(x, xBounds[1])]
         yBounds = [min(y, yBounds[0]), max(y, yBounds[1])]
@@ -37,7 +75,8 @@ class Table: CustomStringConvertible {
     }
 
     func get(_ x: Int, _ y: Int, _ z: Int) -> Character {
-        table[x]?[y]?[z] ?? " "
+        let key = [x, y, z]
+        return grid[key] ?? "."
     }
 
     var description: String {
@@ -53,5 +92,9 @@ class Table: CustomStringConvertible {
             output += "\n"
         }
         return output
+    }
+
+    var numActive: Int {
+        _numActive
     }
 }
