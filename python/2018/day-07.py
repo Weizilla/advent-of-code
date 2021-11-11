a1fa37f63e02c5b0871010cfaa9ab7b8a5be5a35
Wei Yang <wei@weizilla.com>
2021-11-10 22:19:15 -0600

    Finish day 7

--- python/2018/day-07.py
+++ python/2018/day-07.py
@@ -1,28 +1,40 @@
 import re
 from dataclasses import dataclass
 from dataclasses import field
+from typing import Optional
 from typing import Union
 
 from solution import Solution
 
 
-@dataclass(order=True, eq=True)
+@dataclass(order=True)
 class Node:
     id: str
     prev: ["Node"] = field(default_factory=list)
     next: ["Node"] = field(default_factory=list)
 
     def __str__(self) -> str:
-        return f"id={self.id} next={[n.id for n in self.next]} prev={[n.id for n in self.prev]}"
+        return f"id={self.id}"
 
     def __repr__(self) -> str:
-        return f"id={self.id} next={[n.id for n in self.next]} prev={[n.id for n in self.prev]}"
+        return f"id={self.id}"
 
-    def __eq__(self, o: object) -> bool:
-        return self.id == o.id
+    def __eq__(self, o: "Node") -> bool:
+        return o and self.id == o.id
 
     def __hash__(self) -> int:
         return self.id.__hash__()
+
+    @property
+    def seconds(self) -> int:
+        return ord(self.id) - ord("A") + 61
+
+
+@dataclass()
+class Worker:
+    id: int
+    node: Optional[Node] = field(default=None)
+    seconds_left: int = field(default=0)
 
 
 class Day7(Solution):
@@ -66,7 +78,43 @@ class Day7(Solution):
         # print(nodes)
         return nodes.values()
 
-    # def part2(self) -> Union[str, int]:
+    def part2(self) -> Union[str, int]:
+        input = self.read_input()
+        nodes = self.parse_input(input)
+
+        start_nodes = sorted([n for n in nodes if not n.prev])
+
+        path = []
+        to_visit = set(start_nodes)
+
+        curr_time = 0
+        workers = [Worker(i) for i in range(5)]
+
+        while to_visit:
+            for worker in workers:
+                if worker.node:
+                    worker.seconds_left -= 1
+                    if worker.seconds_left == 0:
+                        path.append(worker.node.id)
+                        to_visit.remove(worker.node)
+                        next_visit = [n for n in worker.node.next if
+                            not n.prev or all([np.id in path for np in n.prev])]
+                        to_visit = to_visit.union(next_visit)
+                        worker.node = None
+
+                if not worker.node:
+                    curr_working = [w.node for w in workers]
+                    can_visit = [n for n in to_visit if n not in curr_working]
+                    if can_visit:
+                        curr = sorted(can_visit)[0]
+                        worker.node = curr
+                        worker.seconds_left = curr.seconds
+
+            curr_time += 1
+
+        print("".join(path))
+
+        return curr_time - 1
 
 
 if __name__ == "__main__":
