3574a5464c3ba6ee36897f3e59cd856798d0a63c
Wei Yang <wei@weizilla.com>
2021-11-27 19:45:32 -0600

    Collections

--- typescript/2017/collections.ts
+++ typescript/2017/collections.ts
@@ -3,26 +3,41 @@ interface HashKey {
 }
 
 class HashMap<K extends HashKey, V> {
-  map: Map<string, V>;
+  private _map: Map<string, V>;
+  private _keys: Map<string, K>;
 
   constructor() {
-    this.map = new Map();
+    this._map = new Map<string, V>();
+    this._keys = new Map<string, K>();
+  }
+
+  entries(): Array<[K, V]> {
+    return Array.from(this._map.entries(), ([k, v]) => [this._keys.get(k)!, v]);
+  }
+
+  keys(): Array<K> {
+    return Array.from(this._keys.values());
+  }
+
+  values(): Array<V> {
+    return Array.from(this._map.values());
   }
 
   get(key: K): V | undefined {
-    return this.map.get(key.toString());
+    return this._map.get(key.toString());
   }
 
   set(key: K, value: V) {
-    this.map.set(key.toString(), value);
+    this._map.set(key.toString(), value);
+    this._keys.set(key.toString(), key);
   }
 
   size() {
-    return this.map.size;
+    return this._map.size;
   }
 
   toString(): string {
-    return JSON.stringify(Array.from(this.map.entries()));
+    return JSON.stringify(Array.from(this.entries()));
   }
 }
 
