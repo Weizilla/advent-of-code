cdb14c4468cf6d10e640e467bdb36d00da35ccce
Wei Yang <wei@weizilla.com>
2021-12-02 06:46:56 -0600

    folds

--- typescript/src/2021/day-02.ts
+++ typescript/src/2021/day-02.ts
@@ -24,18 +24,7 @@ class Day2 extends Solution {
   part1(): number | string | undefined {
     const inputs = this.readInput();
     const actions = inputs.map(i => this.parseInput(i));
-    let hor = 0;
-    let depth = 0;
-    actions.forEach(a => {
-      if (a.type === ActionType.FORWARD) {
-        hor += a.amount;
-      } else if (a.type === ActionType.DOWN) {
-        depth += a.amount;
-      } else if (a.type === ActionType.UP) {
-        depth -= a.amount;
-      }
-    });
-
+    const [hor, depth] = actions.reduce(this.reducePart1, [0, 0]);
     return hor * depth;
   }
 
@@ -47,24 +36,36 @@ class Day2 extends Solution {
     return new Action(actionType, actionAmount);
   }
 
+  reducePart1(prev: [number, number], a: Action): [number, number] {
+    let [hor, depth] = prev;
+    if (a.type === ActionType.FORWARD) {
+      hor += a.amount;
+    } else if (a.type === ActionType.DOWN) {
+      depth += a.amount;
+    } else if (a.type === ActionType.UP) {
+      depth -= a.amount;
+    }
+    return [hor, depth];
+  }
+
   part2(): number | string | undefined {
     const inputs = this.readInput();
     const actions = inputs.map(i => this.parseInput(i));
-    let hor = 0;
-    let depth = 0;
-    let aim = 0;
-    actions.forEach(a => {
-      if (a.type === ActionType.FORWARD) {
-        hor += a.amount;
-        depth += a.amount * aim;
-      } else if (a.type === ActionType.DOWN) {
-        aim += a.amount;
-      } else if (a.type === ActionType.UP) {
-        aim -= a.amount;
-      }
-    });
-
+    const [hor, depth] = actions.reduce(this.reducePart2, [0, 0, 0]);
     return hor * depth;
+  }
+
+  reducePart2(prev: [number, number, number], a: Action): [number, number, number] {
+    let [hor, depth, aim] = prev;
+    if (a.type === ActionType.FORWARD) {
+      hor += a.amount;
+      depth += a.amount * aim;
+    } else if (a.type === ActionType.DOWN) {
+      aim += a.amount;
+    } else if (a.type === ActionType.UP) {
+      aim -= a.amount;
+    }
+    return [hor, depth, aim];
   }
 }
 
