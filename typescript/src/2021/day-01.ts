6f8b3b58ad88cda4327902b165e2e7d5862b8af0
Wei Yang <wei@weizilla.com>
2021-12-02 06:25:50 -0600

    Add 2021 day 1

---
+++ typescript/src/2021/day-01.ts
@@ -0,0 +1,33 @@
+import { Solution } from "../solution";
+
+class Day1 extends Solution {
+  constructor(example?: number) {
+    super(1, 2021, example);
+  }
+
+  part1(): number | string | undefined {
+    const inputs = this.readInputInts();
+    let numIncrease = 0;
+    for (let i = 0, j = 1; j < inputs.length; i++, j++) {
+      if (inputs[i] < inputs[j]) {
+        numIncrease += 1;
+      }
+    }
+
+    return numIncrease;
+  }
+
+  part2(): number | string | undefined {
+    const inputs = this.readInputInts();
+    let numIncrease = 0;
+    for (let i = 0, j = 3; j < inputs.length; i++, j++) {
+      if (inputs[i] < inputs[j]) {
+        numIncrease += 1;
+      }
+    }
+
+    return numIncrease;
+  }
+}
+
+(new Day1()).run();
