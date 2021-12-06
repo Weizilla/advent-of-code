770192b614bfeed32b640149ad5ffd6941306b8d
Wei Yang <wei@weizilla.com>
2021-12-06 08:56:40 -0600

    Add range util

--- typescript/src/utils.ts
+++ typescript/src/utils.ts
@@ -4,4 +4,22 @@ function print(input: any, color: string = "black") {
   console.log(chalk.keyword(color)("%s"), input);
 }
 
-export { print };
+function sum(input: Iterable<number>) {
+  return [...input].reduce((p, c) => p + c, 0);
+}
+
+function range(end: number, start: number = 0, includeEnd: boolean = false): number[] {
+  if (start === end) {
+    return [start];
+  }
+
+  const inc = start > end ? -1 : 1;
+  const newEnd = includeEnd ? end + inc : end;
+  const results = [];
+  for (let i = start; i !== newEnd; i += inc) {
+    results.push(i);
+  }
+  return results;
+}
+
+export { print, sum, range };
