7edb7d7de0eb94660aad801b781a3b11e57b68fc
Wei Yang <wei@weizilla.com>
2021-12-14 06:49:22 -0600

    Add more day 14 part 2

--- typescript/src/2021/day-14.ts
+++ typescript/src/2021/day-14.ts
@@ -1,4 +1,19 @@
 import { Solution } from "../solution";
+import { print } from "../utils";
+
+class Node {
+  value: string;
+  next: Node | null;
+
+  constructor(value: string) {
+    this.value = value;
+    this.next = null;
+  }
+
+  toString(): string {
+    return `${this.value} ${this.next?.value || ""}`;
+  }
+}
 
 class Day14 extends Solution {
   constructor(example?: number) {
@@ -68,11 +83,73 @@ class Day14 extends Solution {
     const inputs = this.readInput();
     const rules = this.readRules(inputs);
 
-    let polymer = inputs[0];
-    for (let i = 0; i < 40; i++) {
-      polymer = this.runStep(polymer, rules);
+    const polymer = inputs[0].split("");
+    const head = new Node(polymer[0]);
+    let prev = head;
+    for (let i = 1; i < polymer.length; i++) {
+      const n = new Node(polymer[i]);
+      prev.next = n;
+      prev = n;
     }
-    return this.calcMinMaxDiff(polymer);
+
+    this.printList(head);
+
+    for (let i = 0; i < 40; i++) {
+      const numAdded = this.runStep2(head, rules);
+      print(`${i} Num added ${numAdded}`);
+    }
+
+    this.printList(head);
+
+    return head.value;
+  }
+
+  printList(head: Node) {
+    let output = "";
+    let curr: Node | null = head;
+    while (curr !== null) {
+      output += curr.value;
+      curr = curr.next;
+    }
+    this.print(output);
+  }
+
+  runStep2(head: Node | null, rules: Map<string, string>): number {
+    let numAdded = 0;
+    let curr: Node | null = head;
+    while (curr !== null && curr.next) {
+      const pair = `${curr.value}${curr.next.value}`;
+      const insert = rules.get(pair);
+      if (insert) {
+        const i = new Node(insert);
+        const next = curr.next;
+        curr.next = i;
+        i.next = next;
+        curr = next;
+        numAdded++;
+      } else {
+        curr = curr.next;
+      }
+    }
+    return numAdded;
+  }
+
+  private calcMinMaxDiff2(head: Node) {
+    const counts = new Map<string, number>();
+
+    let curr: Node | null = head;
+    while (curr !== null) {
+      const count = counts.get(curr.value) || 0;
+      counts.set(curr.value, count + 1);
+      curr = curr.next;
+    }
+
+    const maxNum = Array.from([...counts.entries()]).sort(([_1, n1], [_2, n2]) => n2 - n1)[0][1];
+    const minNum = Array.from([...counts.entries()]).sort(([_1, n1], [_2, n2]) => n1 - n2)[0][1];
+
+    this.print(`${minNum} ${maxNum}`);
+
+    return maxNum - minNum;
   }
 }
 
