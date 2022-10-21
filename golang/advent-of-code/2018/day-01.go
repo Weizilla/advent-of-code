935f3fb4e3b89067a3d28a45565b90db79d05087
Wei Yang <wei@weizilla.com>
2022-10-21 10:39:04 -0700

    Add day 1

--- golang/advent-of-code/2018/day-01.go
+++ golang/advent-of-code/2018/day-01.go
@@ -3,20 +3,39 @@ package main
 import (
 	"advent-of-code/solution"
 	"fmt"
-	"strconv"
 )
 
-func day1() string {
-	inputs := solution.ReadInputInts(2018, 1)
+const year = 2018
+const day = 1
+
+func day1() any {
+	inputs := solution.ReadInputInts(year, day)
 	sum := 0
 
 	for _, value := range inputs {
 		sum += value
 	}
 
-	return strconv.Itoa(sum)
+	return sum
+}
+
+func day2() any {
+	inputs := solution.ReadInputInts(year, day)
+
+	seen := map[int]bool{}
+	sum := 0
+	for {
+		for _, value := range inputs {
+			sum += value
+			if seen[sum] {
+				return sum
+			} else {
+				seen[sum] = true
+			}
+		}
+	}
 }
 
 func main() {
-	fmt.Println(day1())
+	fmt.Println(day2())
 }
