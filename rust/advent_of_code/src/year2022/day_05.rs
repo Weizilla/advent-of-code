6a07f4fd3aad1028fb162d647da9f1c5c6ec4dac
Wei Yang <wei@weizilla.com>
2022-12-05 19:12:30 -0800

    Rust day 5

--- rust/advent_of_code/src/year2022/day_05.rs
+++ rust/advent_of_code/src/year2022/day_05.rs
@@ -8,7 +8,7 @@ impl Solution for Day05 {
         SolutionArgs {
             year: 2022,
             day: 5,
-            input_type: InputType::EXAMPLE(1),
+            input_type: InputType::INPUT,
         }
     }
 
@@ -37,11 +37,39 @@ impl Solution for Day05 {
 
         do_move(&mut s, &m);
 
-        None
+        let answer = s.iter().map(|v| v.last().expect("error")).collect();
+
+        Some(answer)
     }
 
     fn part2(&self) -> Option<String> {
-        None
+        let lines = read_lines(self.args());
+
+        let mut setup: Vec<String> = Vec::new();
+        let mut moves: Vec<String> = Vec::new();
+
+        let mut is_setup = true;
+        for line in lines {
+            if line.len() == 0 {
+                is_setup = false;
+                continue;
+            }
+            if is_setup && line.contains("[") {
+                setup.push(line);
+            } else if line.contains("move") {
+                moves.push(line);
+            }
+        }
+
+
+        let mut s = parse_setup(&setup);
+        let m = parse_moves(&moves);
+
+        do_move_2(&mut s, &m);
+
+        let answer = s.iter().map(|v| v.last().expect("error")).collect();
+
+        Some(answer)
     }
 }
 
@@ -63,7 +91,7 @@ fn parse_setup(lines: &Vec<String>) -> Vec<Vec<char>> {
                 }
             }
         }
-
+        stack.reverse();
         all_stacks.push(stack);
     }
 
@@ -86,17 +114,28 @@ fn parse_moves(lines: &Vec<String>) -> Vec<Move> {
 }
 
 fn do_move(setup: &mut Vec<Vec<char>>, moves: &Vec<Move>) {
-    eprintln!("s = {:#?}", setup);
     for m in moves {
-        for i in 0..m.num {
-            let mut f = &mut setup[m.from - 1];
+        for _ in 0..m.num {
+            let f = &mut setup[m.from - 1];
             let c = f.pop().expect("error");
-            let mut t = &mut setup[m.to - 1];
+            let t = &mut setup[m.to - 1];
             t.push(c);
         }
+    }
+}
 
-        eprintln!("m = {:#?}", m);
-        eprintln!("s = {:#?}", setup);
+fn do_move_2(setup: &mut Vec<Vec<char>>, moves: &Vec<Move>) {
+    // eprintln!("setup = {:?}", setup);
+    for m in moves {
+        // eprintln!("moves = {:?}", m);
+        for i in 0..m.num {
+            let f = &mut setup[m.from - 1];
+            // eprintln!("i = {:?}", f.len() - (m.num - i) as usize);
+            let c = f.remove(f.len() - (m.num - i) as usize);
+            let t = &mut setup[m.to - 1];
+            t.push(c);
+        }
+        // eprintln!("s = {:?}", setup);
     }
 }
 
