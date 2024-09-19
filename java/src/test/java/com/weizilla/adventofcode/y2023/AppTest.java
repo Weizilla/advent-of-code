3d1e371dc8cf32f1ce442fd94334e7e724dd1496
Wei Yang <wei@weizilla.com>
2024-09-18 21:06:20 -0700

    Add java skeleton

---
+++ java/src/test/java/com/weizilla/adventofcode/y2023/AppTest.java
@@ -0,0 +1,19 @@
+package com.weizilla.adventofcode.y2023;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
