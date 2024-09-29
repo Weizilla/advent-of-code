c340c77af33fe839da550030491b3f7428f9686b
Wei Yang <wei@weizilla.com>
2024-09-28 21:41:31 -0700

    more runner stuff

--- java/src/main/java/com/weizilla/adventofcode/utils/Runner.java
+++ java/src/main/java/com/weizilla/adventofcode/utils/Runner.java
@@ -1,35 +1,47 @@
 package com.weizilla.adventofcode.utils;
 
+import com.google.common.collect.Iterables;
 import com.google.common.reflect.ClassPath;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.Method;
-import java.util.Set;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class Runner {
     private static final Logger logger = LoggerFactory.getLogger(Runner.class);
 
-
-    private static Set<Class> getAllClasses() throws Exception {
+    private static List<Class> getAllClasses() throws Exception {
         return ClassPath.from(ClassLoader.getSystemClassLoader())
-            .getAllClasses()
-            .stream()
-                .filter(c -> c.getPackageName().contains("com.weizilla.adventofcode.y"))
+            .getAllClasses().stream()
+            .filter(c -> c.getPackageName().contains("com.weizilla.adventofcode.y"))
+            .filter(c -> !c.getName().contains("$"))
             .map(ClassPath.ClassInfo::load)
-            .collect(Collectors.toSet());
+            .sorted(Comparator.comparing(Class::getName))
+            .collect(Collectors.toList());
+    }
+
+    private static List<Method> getAllMethods(Object object) {
+        return Arrays.stream(object.getClass().getDeclaredMethods())
+            .sorted(Comparator.comparing(Method::getName))
+            .toList();
     }
 
     public static void main(String[] args) {
         try {
-            Set<Class> classes = getAllClasses();
-            //TODO
-            Class next = classes.iterator().next();
-            Object i = next.getConstructors()[0].newInstance();
-            Method part1 = Day.class.getMethod("part1");
-            Object invoke = part1.invoke(i);
-            logger.info("Result {}", invoke);
+            List<Class> classes = getAllClasses();
+            Class<?> newestClass = Iterables.getLast(classes);
+            Object instance = newestClass.getConstructors()[0].newInstance();
+
+            List<Method> methods = getAllMethods(instance);
+
+            Method latestMethod = Iterables.getLast(methods);
+            Object result = latestMethod.invoke(instance);
+            logger.info("Running {}.{} Result {}", newestClass.getName(), latestMethod.getName(), result);
+
         } catch (Exception e) {
             logger.error("error", e);
         }
