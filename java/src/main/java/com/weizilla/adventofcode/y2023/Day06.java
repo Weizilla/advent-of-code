f2ab21e5f469fd5aefe989877bec42dec029cc9b
Wei Yang <wei@weizilla.com>
2024-10-17 06:53:26 -0700

    start day 6

---
+++ java/src/main/java/com/weizilla/adventofcode/y2023/Day06.java
@@ -0,0 +1,14 @@
+package com.weizilla.adventofcode.y2023;
+
+import com.weizilla.adventofcode.utils.Day;
+
+public class Day06 extends Day {
+    public Day06(Integer example) {
+        super(example);
+    }
+
+    @Override
+    public Object part1() {
+        return "abc";
+    }
+}
