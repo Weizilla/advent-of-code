2a743a2438e7ee9b4372f40253aa128b0c39b664
Wei Yang <wei@weizilla.com>
2024-12-07 08:04:20 -0800

    Start day 4

---
+++ scala/src/main/scala/y2024/Day04.scala
@@ -0,0 +1,8 @@
+package com.weizilla.adventofcode
+package y2024
+
+import utils.Day
+
+class Day04 (example: Integer) extends Day(2024, 4, example) {
+  override def part1(): AnyRef = super.part1()
+}
