b94ab54244fd4176349474ea7987ffd4186838e3
Wei Yang <wei@weizilla.com>
2024-12-10 21:45:03 -0800

    Add day 5

---
+++ scala/src/main/scala/y2024/Day05.scala
@@ -0,0 +1,10 @@
+package com.weizilla.adventofcode
+package y2024
+
+import utils.Day
+
+class Day05(example: Integer) extends Day(2024, 5, example) {
+  override def part1(): Any = {
+    "A"
+  }
+}
