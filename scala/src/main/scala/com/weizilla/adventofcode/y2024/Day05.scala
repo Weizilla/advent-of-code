ae11b36ec8fed254ab9d6d4b4295831f15ee500e
Wei Yang <wei@weizilla.com>
2024-12-13 19:02:07 -0800

    finish day 5

--- scala/src/main/scala/com/weizilla/adventofcode/y2024/Day05.scala
+++ scala/src/main/scala/com/weizilla/adventofcode/y2024/Day05.scala
@@ -49,4 +49,49 @@ class Day05(example: Integer) extends Day(2024, 5, example) {
 
     sum
   }
+
+  override def part2(): Any = {
+    val ordered = mutable.Set[(Int, Int)]()
+    val reverse = mutable.Set[(Int, Int)]()
+    val updates = mutable.ListBuffer[List[Int]]()
+
+    for (line <- reader.readLines()) {
+      if (line.contains("|")) {
+        val splits = line.split("\\|").map(_.toInt)
+        val a = splits(0)
+        val b = splits(1)
+        ordered.add((a, b))
+        reverse.add((b, a))
+      } else if (line.nonEmpty) {
+        val update: List[Int] = line.split(",").map(_.toInt).toList
+        updates.append(update)
+      }
+    }
+
+    var sum = 0
+    for (update <- updates) {
+      val u = update.sorted(new Day05Comparator(ordered, reverse))
+      if (!u.equals(update)) {
+        print("BEFORE {} update AFTER {}", update, u)
+        sum += u(u.length / 2)
+      }
+    }
+    sum
+  }
+
+  private class Day05Comparator(order: mutable.Set[(Int, Int)], reversed: mutable.Set[(Int, Int)]) extends Ordering[Int] {
+
+    override def compare(x: Int, y: Int): Int = {
+      val o = (x, y)
+      val r = (y, x)
+      if (order.contains(o) || reversed.contains(r)) {
+        return -1
+      } else if (order.contains(r) || reversed.contains(o)) {
+        return 1
+      }
+
+      return 0
+    }
+  }
 }
+
