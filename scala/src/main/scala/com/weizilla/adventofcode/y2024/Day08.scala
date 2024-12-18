7d6592b0dcddf997fa438a2bec0aecba55365fc5
Wei Yang <wei@weizilla.com>
2024-12-18 07:39:49 -0800

    finish day 8

--- scala/src/main/scala/com/weizilla/adventofcode/y2024/Day08.scala
+++ scala/src/main/scala/com/weizilla/adventofcode/y2024/Day08.scala
@@ -4,7 +4,9 @@ import com.weizilla.adventofcode.utils.{Day, Grid, Point}
 
 import scala.collection.mutable
 
+
 class Day08(example: Integer) extends Day(2024, 8, example) {
+
   override def part1(): Any = {
     val grid = reader.readGrid()
     val antennas = grid
@@ -52,5 +54,77 @@ class Day08(example: Integer) extends Day(2024, 8, example) {
     print("{} {} {}", p1, p2, results)
 
     results
+  }
+
+  override def part2(): Any = {
+    val grid = reader.readGrid()
+    val antennas = grid
+      .filter(p => !p._2.equals("."))
+      .groupBy(p => p._2)
+
+    val results = antennas.values.flatMap(i => makeAntiNodes2(i.toList, grid)).toList.distinct
+
+//    print(results)
+
+    results.foreach(p => grid.set(p, "#"))
+    print("\n" ++ grid.prettyPrint())
+
+    results.size
+  }
+
+  private def makeAntiNodes2(points: List[(Point, String)], grid: Grid): mutable.Set[Point] = {
+    print("Points={}", points)
+    val results = mutable.Set[Point]()
+    for (x <- points.indices; y <- points.indices if x != y) {
+      val r = makeAntiNode2(points(x)._1, points(y)._1, grid)
+      results.addAll(r)
+    }
+
+    results
+  }
+
+  private def makeAntiNode2(p1: Point, p2: Point, grid: Grid): List[Point] = {
+    /*
+    (1, 1) (4,5)
+      => (4 - 1) + 4, (5 - 1) + 5
+      => (1 - 4) + 1, (1 - 5) + 1
+
+     (4, 5), (1, 1)
+      => (1 - 4) + 1, (1 - 5) + 1
+      => (4 - 1) + 4, (5 - 1) + 5
+     */
+
+    val results = mutable.ListBuffer[Point]()
+
+
+    var i = 0
+    var x1 = (i * (p2.x - p1.x)) + p2.x
+    var y1 = (i * (p2.y - p1.y)) + p2.y
+    var curr = Point(x1, y1)
+    while (grid.inBounds(curr)) {
+      results.addOne(curr)
+
+      i += 1
+      x1 = (i * (p2.x - p1.x)) + p2.x
+      y1 = (i * (p2.y - p1.y)) + p2.y
+      curr = Point(x1, y1)
+    }
+
+    i = 0
+    x1 = (i * (p1.x - p2.x)) + p1.x
+    y1 = (i * (p1.y - p2.y)) + p1.y
+    curr = Point(x1, y1)
+    while (grid.inBounds(curr)) {
+      results.addOne(curr)
+
+      i += 1
+      x1 = (i * (p1.x - p2.x)) + p1.x
+      y1 = (i * (p1.y - p2.y)) + p1.y
+      curr = Point(x1, y1)
+    }
+
+    print("p1={} p2={} {}", p1, p2, results)
+
+    results.toList
   }
 }
