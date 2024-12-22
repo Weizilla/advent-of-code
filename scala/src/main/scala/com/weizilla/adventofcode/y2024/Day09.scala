79e304e219d2cd216bf4ea9719ea873bbbcd7d2c
Wei Yang <wei@weizilla.com>
2024-12-21 21:14:03 -0800

    finish day 9 part 1

--- scala/src/main/scala/com/weizilla/adventofcode/y2024/Day09.scala
+++ scala/src/main/scala/com/weizilla/adventofcode/y2024/Day09.scala
@@ -8,24 +8,65 @@ class Day09(example: Integer) extends Day(2024, 9, example) {
   override def part1(): Any = {
     val line = reader.readLines().head
 
-    val blocks = mutable.ListBuffer[Block]()
+    val fileBlocks = mutable.ArrayDeque[FileBlock]()
+    val freeBlocks = mutable.ArrayDeque[FreeBlock]()
+
     var id = 0
     var index = 0
     for ((value, i) <- line.split("").map(_.toInt).zipWithIndex if value != 0) {
       if (i % 2 == 0) {
         val block = FileBlock(index, value, id)
-        blocks.addOne(block)
+        fileBlocks.addOne(block)
         id += 1
       } else {
         val block = FreeBlock(index, value)
-        blocks.addOne(block)
+        freeBlocks.addOne(block)
       }
       index += value
     }
 
-    blocks.sorted
 
-    blocks
+    while (freeBlocks.nonEmpty) {
+      val free = freeBlocks.removeHead()
+      val file = fileBlocks.removeLast()
+
+      if (free.length < file.length) {
+        val diff = file.length - free.length
+
+        val newFile = FileBlock(free.start, free.length, file.id)
+        fileBlocks.addOne(newFile)
+
+        val rem = FileBlock(file.start, diff, file.id)
+        fileBlocks.addOne(rem)
+
+      } else if (free.length > file.length) {
+        val diff = free.length - file.length
+
+        val newFile = FileBlock(free.start, file.length, file.id)
+        fileBlocks.addOne(newFile)
+
+        val newFree = FreeBlock(free.start + file.length, diff)
+        freeBlocks.addOne(newFree)
+
+      } else {
+        val newBlock = FileBlock(free.start, free.length, file.id)
+        fileBlocks.addOne(newBlock)
+      }
+      fileBlocks.sortInPlace()
+      freeBlocks.sortInPlace()
+    }
+
+    val compacted = mutable.ArrayDeque[FileBlock]()
+    var end = 0
+    for (block <- fileBlocks) {
+      compacted.addOne(FileBlock(end, block.length, block.id))
+      end += block.length
+    }
+
+    print(compacted)
+
+    compacted.map(_.checksum()).sum
+
   }
 
   private trait Block extends Ordered[Block] {
@@ -39,6 +80,18 @@ class Day09(example: Integer) extends Day(2024, 9, example) {
   }
 
   private case class FileBlock(start: Int, length: Int, id: Int) extends Block with Ordered[Block] {
+    def checksum(): Long = {
+      // 10 + 11 + 12 + 13 + 15 + 16 = (10 + 13) * (len / 2)
+      // 10 + 11 + 12 + 13 + 14 + 16 + 17 = (10 + 14) * ((len / 2) + 0.5) +
+
+      val maybeHalf = if (length % 2 == 1) 0.5 else 0.0
+      val blockNums = ((start + (end - 1)) * ((length / 2) + maybeHalf)).toLong
+      blockNums * id
+    }
+
+    override def toString: String = {
+      s"$start->$end($id)[${checksum()}]"
+    }
   }
 
   private case class FreeBlock(start: Int, length: Int) extends Block { }
