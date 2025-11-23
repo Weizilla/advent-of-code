08114d15b1e939b5c22a433a3fd4f828d2136294
Wei Yang <wei@weizilla.com>
2025-11-23 07:36:53 -0800

    start day 2 part 2

--- ruby/2024/day02.rb
+++ ruby/2024/day02.rb
@@ -24,6 +24,29 @@ module Day02
       diffs.all? { |d| d.between?(1, 3) } || diffs.all? { |d| d.between?(-3, -1) }
     end
   end
+
+  class Part2
+    include Util
+
+    def run
+      lines = read_input_lines
+      safes = lines.map do |line|
+        safe?(line)
+      end
+
+      safes.count { |s| s }
+    end
+
+    private
+
+    # @param [String] line
+    def safe?(line)
+      levels = line.split.map(&:to_i)
+      diffs = levels.each_cons(2).map { |a, b| b - a }
+      diffs.count { |d| d.between?(1, 3) } >= diffs.length - 1 ||
+        diffs.count { |d| d.between?(-3, -1) } >= diffs.length - 1
+    end
+  end
 end
 
-puts Day02::Part1.new.run
+puts Day02::Part2.new.run
