3a2a95368c870e0698ab1d792576c8198e2ca220
Wei Yang <wei@weizilla.com>
2025-11-23 21:31:19 -0800

    day 3 part 2, update rubocop

--- ruby/2024/day03.rb
+++ ruby/2024/day03.rb
@@ -12,8 +12,33 @@ module Day03
       match = line.scan(/mul\((\d{1,3}),(\d{1,3})\)/)
       match.sum { |a, b| a.to_i * b.to_i }
     end
+  end
 
+  class Part2
+    include Util
+
+    def run
+      line = read_input_lines.join
+
+      sum = 0
+      accept = true
+
+      match = line.scan(/mul\((\d{1,3}),(\d{1,3})\)|(don't\(\))|(do\(\))/)
+      match.each do |num1, num2, donot_found, do_found|
+        if num1 && num2
+          sum += num1.to_i * num2.to_i if accept
+        elsif do_found
+          accept = true
+        elsif donot_found
+          accept = false
+        else
+          raise(StandardError)
+        end
+      end
+
+      sum
+    end
   end
 end
 
-p Day03::Part1.new.run
+p Day03::Part2.new.run
