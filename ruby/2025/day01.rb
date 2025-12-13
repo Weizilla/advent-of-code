1e3530a6f0eb938332a32feaf535b78a6ffb1957
Wei Yang <wei@weizilla.com>
2025-12-13 14:31:53 -0800

    start 2025

---
+++ ruby/2025/day01.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require_relative '../util'
+
+module Year2024
+  module Day01
+    class Part1
+      include Util
+
+      def run
+        0
+      end
+    end
+  end
+end
+
+p Year2024::Day01::Part1.new.run
