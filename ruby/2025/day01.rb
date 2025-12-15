abd2ab0e099187ca2b1b125592c285f2389dcca8
Wei Yang <wei@weizilla.com>
2025-12-14 21:47:21 -0800

    start day 1

--- ruby/2025/day01.rb
+++ ruby/2025/day01.rb
@@ -8,10 +8,59 @@ module Year2024
       include Util
 
       def run
-        0
+        curr = 50
+        num_zeros = 0
+
+        lines = read_input_lines
+        lines.each do |l|
+          num = l[1..].to_i
+          if l.start_with?("L")
+            num *= -1
+          end
+          curr += num
+          while curr >= 100
+            curr -= 100
+          end
+          while curr < 0
+            curr += 100
+          end
+          if curr == 0
+            num_zeros += 1
+          end
+        end
+        num_zeros
+      end
+    end
+
+    class Part2
+      include Util
+
+      def run
+        curr = 50
+        num_zeros = 0
+
+        lines = read_input_lines
+        lines.each do |l|
+          num = l[1..].to_i
+          if l.start_with?("L")
+            num *= -1
+          end
+          prev = curr
+          curr += num
+          while curr >= 100
+            curr -= 100
+          end
+          while curr < 0
+            curr += 100
+          end
+          if curr == 0
+            num_zeros += 1
+          end
+        end
+        num_zeros
       end
     end
   end
 end
 
-p Year2024::Day01::Part1.new.run
+p Year2024::Day01::Part2.new.run
